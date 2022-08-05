434dcb2239a8c09202805e1ebedb70304e8a27fc
Szymon164 <szymonrecko1@gmail.com>
2022-08-05 17:46:11 +0200

    Deleting stocks.py

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import pandas
 from fastapi import FastAPI
-from stocks import get_stocks
 
 app = FastAPI()
 
 @app.get('/')
 def root():
-    tmp = get_stocks()
-    return tmp
+    return {'message':'Hello World!'}
 
